8528df2f21bfcb061b712eee9dea92f283b64a0b
gum <huy.vt00578@sinhvien.hoasen.edu.vn>
2025-01-20 22:34:06 +0700

    feauture: [auth] define app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,30 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { AuthGuard, NonAuthGuard } from "@app/core/guards";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    data: { title: 'Home' },
+    path: "",
     loadChildren: () =>
-      import('./features/home/home.module').then((m) => m.HomeModule),
+      import("./features/home/home.module").then((m) => m.HomeModule),
   },
   {
-    path: 'auth',
-    data: { title: 'Auth' },
+    path: "auth",
     loadChildren: () =>
-      import('./features/auth/auth.module').then((m) => m.AuthModule),
+      import("./features/auth/auth.module").then((m) => m.AuthModule),
+    canActivate: [NonAuthGuard],
+  },
+  {
+    path: "editor",
+    loadChildren: () =>
+      import("./features/editor/editor.module").then((m) => m.EditorModule),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: "profile",
+    loadChildren: () =>
+      import("./features/profile/profile.module").then((m) => m.ProfileModule),
+    canActivate: [AuthGuard],
   },
 ];
 
