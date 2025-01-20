b35b6a7df7d9ceb4944efa7be1c771d8f538eaad
gum <huy.vt00578@sinhvien.hoasen.edu.vn>
2025-01-20 22:48:57 +0700

    fix: [configs] fix typo prefix, translation config and app main layout css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from '@app/app.component';
-import { HeaderModule } from '@app/shared/components/header/header.module';
-import { FooterModule } from '@app/shared/components/footer/footer.module';
+import { AppComponent } from "@app/app.component";
+import { HeaderModule } from "@app/shared/components/header/header.module";
+import { FooterModule } from "@app/shared/components/footer/footer.module";
 
-import { ToastrModule } from 'ngx-toastr';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { AppRoutingModule } from '@app/app-routing.module';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from "ngx-toastr";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { AppRoutingModule } from "@app/app-routing.module";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -29,7 +29,7 @@ export function HttpLoaderFactory(http: HttpClient) {
       preventDuplicates: true,
     }),
     TranslateModule.forRoot({
-      defaultLanguage: 'en',
+      defaultLanguage: "en",
       loader: {
         provide: TranslateLoader,
         useFactory: HttpLoaderFactory,
